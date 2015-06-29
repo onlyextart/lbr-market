d6736a2e12ba11f0ccbcec94925d5aa18709a4e3
Vasiliyan Yury <vasiliyan@lbr.ru>
2015-06-29 11:47:34 +0300

    edit view mail

--- protected/views/mail/mail_cart.php
+++ protected/views/mail/mail_cart.php
@@ -89,7 +89,7 @@
             <table rules="none" style="width:600px">
                 <tbody>
                     <tr>
-                        <td><a href="http://lbr-market.ru/" target="_blank" style="margin: 0; padding: 0; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: justify; color: black; "><strong>www.lbr-market.ru</strong></a> – широкий ассортимент запчастей для</div><div>сельскохозяйственной техники.</div><div>тел. для всех регионов России: +8 800 555 32 19</td>
+                        <td><a href="http://lbr-market.ru/" target="_blank" style="margin: 0; padding: 0; font-family: 'Trebuchet MS', sans-serif; font-size: 14px; font-weight: normal; text-align: justify; color: black; "><strong>www.lbr-market.ru</strong></a> – широкий ассортимент запчастей для</div><div>сельскохозяйственной техники.</div><div>тел. для всех регионов России: 8-800-5553219</td>
                         <td style="float:right">&nbsp;<img src="http://lbr-market.ru/images/mail/tractor.png" /></td>
                     </tr>
                     <tr>
