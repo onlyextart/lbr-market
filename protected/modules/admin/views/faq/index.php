447841b8761247ad163482a3e06976d3a3b909e7
Tatsyana Krylova <krilova@lbr.ru>
2015-07-09 11:11:49 +0300

    faq update

--- protected/modules/admin/views/faq/index.php
+++ protected/modules/admin/views/faq/index.php
@@ -8,25 +8,35 @@
     </div>
     <p>Разделы:</p>
     <div class="faq-sections">
-        <h4><a name="meta">1. Добавление/редактирование meta-title, meta-description</a></h4>
+        <h4><a name="meta">1. Добавление/редактирование meta-title, meta-description, seo-тексты (верхиний/нижний)</a></h4>
         <div class="info">
-            <p>Meta-title и meta-description указываются разделах:</p>
+            <p>Meta-title и meta-description, seo-тексты (верхиний/нижний) указываются разделах:</p>
             <ul class="list-of-items">
-                <li><a href="/admin/category/" target="_blank">Категории</a></li>
-                <li><a href="/admin/equipmentmaker/" target="_blank">Производители техники</a></li>
-                <li><a href="/admin/modelline/" target="_blank">Модельные ряды</a></li>
+                <li>
+                    <a href="/admin/category/" target="_blank">Категории</a>
+                    <div>
+                        Пример: <a href="/catalog/traktornaya-tehnika/buhler-versatile-inc/" target="_blank">Тракторная техника</a>, <a href="/catalog/traktornaya-tehnika/traktory/" target="_blank">Тракторы</a>
+                    </div>
+                </li>
+                <li>
+                    <a href="/admin/equipmentmaker/" target="_blank">Производители техники (общее описание)</a>
+                    <div>
+                        Пример: <a href="/manufacturer/buhler-versatile-inc/" target="_blank">Buhler Versalite Inc.</a>
+                    </div>
+                </li>
+                <li>
+                    <a href="/admin/categoryseo/" target="_blank">Производители техники в категории</a>
+                    <div>
+                        Пример: <a href="http://lbr-market.test/catalog/traktornaya-tehnika/traktory/buhler-versatile-inc/" target="_blank">Тракторы - Buhler Versalite Inc.</a>
+                    </div>
+                </li>
+                <li>
+                    <a href="/admin/modelline/" target="_blank">Модельные ряды</a>
+                    <div>
+                        Пример: <a href="/catalog/traktornaya-tehnika/traktory/buhler-versatile-inc/buhler-versatile-2240-2270-2290/" target="_blank">Модельный ряд "BUHLER VERSATILE 2240,2270,2290"</a>
+                    </div>
+                </li>
             </ul>
-            <div>
-                <p>
-                    Категории - пример: <a href="/catalog/traktornaya-tehnika/buhler-versatile-inc/" target="_blank">Тракторная техника</a></li>, <a href="/catalog/traktornaya-tehnika/traktory/" target="_blank">Тракторы</a></li>
-                </p>
-                <p>
-                    Производители техники - пример: <a href="/manufacturer/buhler-versatile-inc/" target="_blank">Buhler Versalite Inc.</a></li>
-                </p>
-                <p>
-                    Модельные ряды - пример: <a href="/catalog/traktornaya-tehnika/traktory/buhler-versatile-inc/buhler-versatile-2240-2270-2290/" target="_blank">Модельный ряд "BUHLER VERSATILE 2240,2270,2290"</a></li>
-                </p>
-            </div>
         </div>
     </div>
 </div>
