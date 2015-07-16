5464957da2a942e3609d82f07810239d42f91390
Tatsyana Krylova <krilova@lbr.ru>
2015-07-16 10:35:29 +0300

    update for site/index.php

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -7,41 +7,24 @@
 <div id="carousel-wrapper">
     <div id="carousel">
         <ul>
-            <?php foreach($bestoffer as $offer){
+            <?php foreach($bestoffer as $offer):
                 $link="/seasonalsale/index/id/".$offer->id;
-                if(file_exists(Yii::getPathOfAlias('webroot').$offer->img)){
-             ?>
-            <li><a href="<?php echo $link ?>"><img src="<?php echo $offer->img ?>" alt="<?php echo $offer->name ?>"></a><!--div class="overlay"><div class="overlay-capture">Спецпредложение 3</div><div class="overlay-text">Моторные, трансмиссионные, гидравлические, масла, тормозные и охлаждающие жидкости, консистентные смазки. Разработаны совместно с CASE IH.</div></div--></li>
-            <!--li><a href="<?php echo $link ?>"><img src="<?php echo $offer->img ?>" alt="<?php echo $offer->name ?>" ></a/--><!--div class="overlay"><div class="overlay-capture">Спецпредложение 3</div><div class="overlay-text">Моторные, трансмиссионные, гидравлические, масла, тормозные и охлаждающие жидкости, консистентные смазки. Разработаны совместно с CASE IH.</div></div--></li>
-            <!--li><img src="/images/sale/5.png" alt="" /--><!--div class="overlay"><div class="overlay-capture">Спецпредложение 3</div><div class="overlay-text">Моторные, трансмиссионные, гидравлические, масла, тормозные и охлаждающие жидкости, консистентные смазки. Разработаны совместно с CASE IH.</div></div--></li>
-            <!--li><img src="/images/sale/3.jpg" alt="" /--><!--div class="overlay"><div class="overlay-capture">Спецпредложение 3</div><div class="overlay-text">Моторные, трансмиссионные, гидравлические, масла, тормозные и охлаждающие жидкости, консистентные смазки. Разработаны совместно с CASE IH.</div></div--></li>
-             <?php } } ?>
+                //if(file_exists(Yii::getPathOfAlias('webroot').$offer->img)){
+            ?>
+            <li><a href="<?php echo $link ?>"><img src="<?php echo $offer->img ?>" alt="<?php echo $offer->name ?>"></a></li>
+            <?php //}  ?>
+            <?php endforeach;  ?>
         </ul>
         <div class="clearfix"></div>
-        <!--a id="prev" class="prev" href="#">&lt;</a>
-        <a id="next" class="next" href="#">&gt;</a-->
         <div id="pager" class="pager"></div>
     </div>
 </div>
 <?php endif; ?>
-<!--div id="carousel-wrapper">
-    <div id="carousel">
-        <ul>
-            <li><img src="/images/sale/4.png" alt="" /></li>
-            <li><img src="/images/sale/5.png" alt="" /></li>
-            <li><img src="/images/sale/3.jpg" alt="" /></li>
-        </ul>
-        <div class="clearfix"></div>
-        <div id="pager" class="pager"></div>
-    </div>
-</div-->
 <div id="shop-description">
     <div>За 15 лет работы <span>&laquo;ЛБР-АгроМаркет&raquo; </span> зарекомендовал себя надежным, заслуживающим доверие клиентов партнером в сфере поставки сельхозтехники и запчастей к ней. Компания продолжает развиваться, совершенствовать свои бизнес-процессы, завоевывая доверия новых клиентов.</div>
     <div><span>Lbr-market.ru </span>  – это электронный магазин запчастей для сельхозтехники, предоставляющий широкий ассортимент продукции (более 60 000 позиций от отечественных и импортных производителей). Вы всегда подберете здесь товар, который будет отличаться высоким качеством и приемлемой стоимостью.</div>
     <div>Продуманная структура сайта, позволяет осуществлять быстрый поиск нужной позиции. Все товары в <span>&laquo;ЛБР-АгроМаркет&raquo; </span> классифицированы по виду техники и производителю. К каждой сельхоз запчасти прилагается техническое описание, информация о производителе и изображение на линейке, что позволяет сопоставить реальные размеры детали.</div>
     <div>Магазин сельхоз запчастей стремится соответствовать требованиям каждого заказчика. Достаточно сделать заказ, а об остальном позаботимся мы. </div>
-    
-
 </div>
 <?php if(!empty($hitProducts)): ?>
 <span class="hit-label-main">Хиты продаж</span>
@@ -62,7 +45,6 @@
 <div id="carousel-logo-wrapper">
     <div id="carousel-logo" class="jcarousel">
         <ul>
-            <!--li><a href="<?php //echo $this->createUrl('description/maker', array('id'=>4)) ?>" class="img-container bwWrapper"><img src="/images/testMakers/Logo-BARBIER.jpg" alt="" /></a></li-->
             <?php
                 $path = Yii::getPathOfAlias('webroot'); 
                 $makers=EquipmentMaker::model()->getAllMakers();
@@ -83,52 +65,34 @@
                 }
 
             ?>
-            
         </ul>
         <div class="clearfix"></div>
-       <a id="prev-logo" class="prev" href="#">&lt;</a>
-       <a id="next-logo" class="next" href="#">&gt;</a> 
-       <!--div id="pager-logo" class="pager"></div-->
+        <a id="prev-logo" class="prev" href="#">&lt;</a>
+        <a id="next-logo" class="next" href="#">&gt;</a>
     </div>
 </div>
 <div class="clearfix"></div>
 <script>
     $(function() {
         $('#carousel ul').carouFredSel({
-           /* prev: '#prev',
-            next: '#next',*/
             pagination: "#pager",
             items: 1,
-            /*scroll: 10000000000,*/
             scroll: 2000,
         });
         
         $('#carousel-logo ul').carouFredSel({
             next: '#next-logo',
             prev: '#prev-logo',
-            //auto: false,
             auto: {
                 items           : 5,
                 fx              :"scroll",
                 easing          : "linear",
                 duration        : 1000,
-                //timeoutDuration :   1000,
                 pauseOnHover    : true,
             },
            pagination: "#pager-logo",
            items: 5,
         });
-
-//        $('.jcarousel').jcarousel({
-//            wrap:"circular", 
-//            animation: {'easing': 'linear', 'duration': 10000} 
-//        });
-//        $('.jcarousel').jcarouselAutoscroll({
-//            interval: 0,
-//            //target: '+=1',
-//            autostart: true
-//        });
-
         
         // black and white for makers' photos
         /*if (navigator.userAgent.search(/Firefox/) > -1){
@@ -150,7 +114,5 @@
         <?php elseif ($MsgErr) :?>
             alertify.error('<?php echo $MsgErr; ?>');
         <?php endif; ?>
-       
     });
-   
 </script>
