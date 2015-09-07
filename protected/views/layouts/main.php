8b051106b4a15d798e947b4fa16e124a5f77f618
Tatsyana Krylova <krilova@lbr.ru>
2015-09-07 14:31:16 +0300

    update style

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -15,7 +15,7 @@
             var lbrAnaliticsMark = "<?php echo Yii::app()->params['analiticsMark']; ?>";
         </script>
         <?php
-            Yii::app()->clientScript->registerCssFile('/distribution/css/styles.min.css?5');
+            Yii::app()->clientScript->registerCssFile('/distribution/css/styles.min.css?6');
             Yii::app()->clientScript->registerCoreScript('jquery');
             Yii::app()->clientScript->registerScriptFile('/distribution/js/scripts.min.js');
 
