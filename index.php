dfcb8458d14771ae6aeb4e1afeeb1667758fa18a
Tatsyana Krylova <krilova@lbr.ru>
2015-09-09 10:42:12 +0300

    update

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+//error_reporting(-1);
+//ini_set('display_errors', true);
+
+$start = microtime(true);
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+
+// remove the following lines when in production mode
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+
+//require_once(dirname(__FILE__).'/../../../../../../usr/share/pear/Mail.php');
+//require_once('/usr/share/pear/Mail/mime.php');  
+Yii::createWebApplication($config)->run();
