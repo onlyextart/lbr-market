eab23bf9c36bf9818367dcca01c85e60b4a6bcb1
Tatsyana Krylova <krilova@lbr.ru>
2015-09-09 10:49:15 +0300

    update

--- protected/modules/admin/controllers/TestController.php
+++ protected/modules/admin/controllers/TestController.php
@@ -5,7 +5,7 @@ class TestController extends Controller
     public function actionIndex()
     {
         //ini_set('memory_limit','2048M');
-        $memory = (int)ini_get("memory_limit");
-        echo ' -> '.$memory;
+        //$memory = (int)ini_get("memory_limit");
+        //echo ' -> '.$memory;
     }
 }
