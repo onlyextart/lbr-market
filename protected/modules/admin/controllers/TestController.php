b49306bface12cca3f4de2cd44319b8e6dc4ffc4
Tatsyana Krylova <krilova@lbr.ru>
2015-09-16 11:07:31 +0300

    del top_text, bottom_text in category, category_seo, equipment_maker

--- protected/modules/admin/controllers/TestController.php
+++ protected/modules/admin/controllers/TestController.php
@@ -4,8 +4,33 @@ class TestController extends Controller
 {
     public function actionIndex()
     {
-        //ini_set('memory_limit','2048M');
-        //$memory = (int)ini_get("memory_limit");
-        //echo ' -> '.$memory;
+        $models = Category::model()->findAll();
+        foreach($models as $model){
+            $model->top_text = null;
+            $model->bottom_text = null;
+            $model->saveNode();
+        }
+        
+        $models = CategorySeo::model()->findAll();
+        foreach($models as $model){
+            $model->top_text = null;
+            $model->bottom_text = null;
+            $model->save();
+        }
+        
+        $models = EquipmentMaker::model()->findAll();
+        foreach($models as $model){
+            $model->top_text = null;
+            $model->bottom_text = null;
+            $model->save();
+        }
+        
+        $prod = Product::model()->findByPk(69109);
+        $prod->date_sale_off = null;
+        $prod->save();
+        
+        $prod = Product::model()->findByPk(68983);
+        $prod->date_sale_off = null;
+        $prod->save();
     }
 }
