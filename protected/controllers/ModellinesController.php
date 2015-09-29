040e0f996a66a56754bee7ef82978c9b3e87c775
Tatsyana Krylova <krilova@lbr.ru>
2015-09-29 11:25:22 +0300

    modellines

--- protected/controllers/ModellinesController.php
+++ protected/controllers/ModellinesController.php
@@ -78,15 +78,40 @@ class ModellinesController extends Controller
         }*/
         $result = $this->setMakerFilter($maker, $count, $id, $categoryRoot->name);
         $dependency = new CDbCacheDependency('SELECT MAX(update_time) FROM model_line');
-        foreach($result as $res){
-            $modelline = $res[0]; 
-            $response .= $this->setModelline($modelline, $dependency, $categoryRoot);
+        
+        $count = count($result);
+        $half = ceil($count/2);
+        
+        ////////////////////////
+//        echo '<pre>';
+//        var_dump($result);
+//        exit;
+        ///////////////////////
+        
+        if(!empty($result)) {
+            $response .= '<table cellspacing="0" cellpadding="0" border="0"><tbody>';
+            for($index = 0; $index < ($half); $index++) {
+                $response .= '<tr>';
+                $response .= '<td width="50%" valign="top" align="left">';
+                $response .= $this->setModelline($result[$index][0], $dependency, $categoryRoot);
+                $response .= '</td>';
+                if(($index + $half) < $count){
+                    $response .= '<td width="50%" valign="top" align="left">';
+                    $response .= $this->setModelline($result[$index + $half][0], $dependency, $categoryRoot);
+                    $response .= '</td>';
+                }
+                $response .= '</tr>';
+            }
+            $response .= '</tbody></table>';
+            
+            /*foreach($result as $res) {
+                $response .= $this->setModelline($res[0], $dependency, $categoryRoot);
+            }*/
         }
         
-        /*echo '<pre>';
-        var_dump($result);
-        exit;
         
+        
+        /*
         if(!empty($result[0])) {
             $modelline = $result[0];     
             //ksort($modelline);
@@ -102,10 +127,10 @@ class ModellinesController extends Controller
     private function setModelline($modelline, $dependency, $categoryRoot)
     {
         $response = '';
-        if(!empty($modelline) && empty(Yii::app()->params['currentMaker'])) {
+        //if(!empty($modelline) && empty(Yii::app()->params['currentMaker'])) {
             foreach($modelline as $categoryName=>$models) {
                 $response .= '<div class="sub-title">'.$categoryName.'</div>';
-                $response .= '<table cellspacing="0" cellpadding="0" border="0"><tbody>';
+                /*$response .= '<table cellspacing="0" cellpadding="0" border="0"><tbody>';
                 $count = 0;
                 $dividend = 3;
                 
@@ -143,18 +168,14 @@ class ModellinesController extends Controller
                   }
                 }
 
-                $response .= '</tbody></table>';
+                $response .= '</tbody></table>';*/
             }
-        } else if(!empty($modelline)){
-            foreach($modelline as $categoryName=>$models) {
-                //$makerH1 = EquipmentMaker::model()->find('name=:name', array(':name'=>$categoryName))->h1;
-                
+        /*} else if(!empty($modelline)) {
+            foreach($modelline as $categoryName => $models) {
                 $title = $categoryName;
-                //if(!empty($makerH1)) $title = $makerH1;
+                $response .= '<h1>'.$title.'</h1>';
                 
-                $response .= '<h1>'.$title.'</h1>
-                   <table cellspacing="0" cellpadding="0" border="0"><tbody>'
-                ;
+                $response .= '<table cellspacing="0" cellpadding="0" border="0"><tbody>';
                 
                 $count = 0;
                 $dividend = 3;
@@ -195,9 +216,109 @@ class ModellinesController extends Controller
 
                 $response .= '</tbody></table>';
             }
-        }
+        }*/
         return $response;
     }
+    
+//    private function setModelline($modelline, $dependency, $categoryRoot)
+//    {
+//        $response = '';
+//        if(!empty($modelline) && empty(Yii::app()->params['currentMaker'])) {
+//            foreach($modelline as $categoryName=>$models) {
+//                $response .= '<div class="sub-title">'.$categoryName.'</div>';
+//                $response .= '<table cellspacing="0" cellpadding="0" border="0"><tbody>';
+//                $count = 0;
+//                $dividend = 3;
+//                
+//                usort($models, array($this, 'sortByName'));
+//                
+//                foreach($models as $model) {
+//                    $category = ModelLine::model()->cache(1000, $dependency)->findByPk($model['id']);
+//                    $children = $category->children()->findAll();
+//
+//                    $count++;
+//                    if($count == 1) $response .= '<tr>';
+//                    
+//                    $response .= '<td valign="top">'.
+//                          '<div class="grey">'.
+//                            '<ul class="accordion modelline">'.
+//                              '<li>'.
+//                                 '<a href="#">'.$model['name'].'</a>'.
+//                                 '<ul>'
+//                    ;
+//
+//                    foreach($children as $child) {
+//                       $brand = EquipmentMaker::model()->findByPk($child->maker_id)->path;
+//                       $response .= '<li><a href="/catalog'.$categoryRoot->path.$brand.$child->path.'/">'.$child->name.'</a></li>';
+//                    }
+//
+//                    $response .= '</ul>'.
+//                              '</li>'.
+//                            '</ul>'.
+//                          '</div>'.
+//                    '</td>'
+//                  ;
+//                  if($count == $dividend) {
+//                      $count = 0;
+//                      $response .= '</tr>';
+//                  }
+//                }
+//
+//                $response .= '</tbody></table>';
+//            }
+//        } else if(!empty($modelline)){
+//            foreach($modelline as $categoryName=>$models) {
+//                //$makerH1 = EquipmentMaker::model()->find('name=:name', array(':name'=>$categoryName))->h1;
+//                
+//                $title = $categoryName;
+//                //if(!empty($makerH1)) $title = $makerH1;
+//                
+//                $response .= '<h1>'.$title.'</h1>
+//                   <table cellspacing="0" cellpadding="0" border="0"><tbody>'
+//                ;
+//                
+//                $count = 0;
+//                $dividend = 3;
+//                
+//                usort($models, array($this, 'sortByName'));
+//                
+//                foreach($models as $model) {
+//                    $category = ModelLine::model()->cache(1000, $dependency)->findByPk($model['id']);
+//                    $children = $category->children()->findAll();
+//
+//                    $count++;
+//                    if($count == 1) $response .= '<tr>';
+//                    
+//                    $response .= '<td valign="top">'.
+//                          '<div class="grey">'.
+//                            '<ul class="accordion modelline">'.
+//                              '<li>'.
+//                                 '<a href="#">'.$model['name'].'</a>'.
+//                                 '<ul>'
+//                    ;
+//
+//                    foreach($children as $child) {
+//                       $brand = EquipmentMaker::model()->findByPk($child->maker_id)->path;
+//                       $response .= '<li><a href="/catalog'.$categoryRoot->path.$brand.$child->path.'/">'.$child->name.'</a></li>';
+//                    }
+//
+//                    $response .= '</ul>'.
+//                              '</li>'.
+//                            '</ul>'.
+//                          '</div>'.
+//                    '</td>'
+//                  ;
+//                  if($count == $dividend) {
+//                      $count = 0;
+//                      $response .= '</tr>';
+//                  }
+//                }
+//
+//                $response .= '</tbody></table>';
+//            }
+//        }
+//        return $response;
+//    }
     
     private function setHitProducts($id)
     {   
@@ -269,12 +390,11 @@ class ModellinesController extends Controller
                     $criteria->params = array(':category_id' => $categoryId);
             }
             
-            if(empty(Yii::app()->params['currentMaker'])){
+            //if(empty(Yii::app()->params['currentMaker'])){
                 $criteriaForBrand = $criteria;
                 $criteriaForBrand->distinct = true;
                 $criteriaForBrand->select = 'maker_id';
                 
-                //$allBrands = ModelLine::model()->cache(1000, $dependency)->findAll($criteriaForBrand);
                 $allBrands = ModelLine::model()->findAll($criteriaForBrand);
                 foreach($allBrands as $brand) {
                    $criteria->distinct = false;
@@ -283,22 +403,20 @@ class ModellinesController extends Controller
                    $criteria->addCondition('category_id = :category_id');
                    $criteria->params = array(':maker_id' => $brand->maker_id, ':category_id' => $categoryId);
                    $name = EquipmentMaker::model()->findByPk($brand->maker_id)->name;
-                   
-                   //$models = ModelLine::model()->cache(1000, $dependency)->findAll($criteria);
+
                    $models = ModelLine::model()->findAll($criteria);
-                   $result[$brand->maker_id] = $this->fillArray($models, $count, $name);
+                   
+                   $result[] = $this->fillArray($models, $count, $name);
                 }
-            } else {
-                //$models = ModelLine::model()->cache(1000, $dependency)->findAll($criteria);
+            /*} else {
                 $models = ModelLine::model()->findAll($criteria);
                 $name = EquipmentMaker::model()->findByPk(Yii::app()->params['currentMaker'])->name;
                 $nameForCategoryInBrand = CategorySeo::model()->find('category_id=:category and equipment_id=:equipment', array('category'=>$categoryId, 'equipment'=>Yii::app()->params['currentMaker']))->h1;
                 if(!empty($nameForCategoryInBrand)) $name = $nameForCategoryInBrand;
                 if(!empty($name))
-                    $result[Yii::app()->params['currentMaker']] = $this->fillArray($models, $count, $name);
-                    //$result[Yii::app()->params['currentMaker']] = $this->fillArray($models, $count, $title);
-                else $result[Yii::app()->params['currentMaker']] = $this->fillArray($models, $count);
-            }
+                    $result[] = $this->fillArray($models, $count, $name);
+                else $result[] = $this->fillArray($models, $count);
+            }*/
         }
         
         return $result;
@@ -308,9 +426,8 @@ class ModellinesController extends Controller
     {   
         $modelline = array();
         $label = $title;
-        //$dependency = new CDbCacheDependency('SELECT MAX(update_time) FROM model_line');
+
         foreach($models as $model) {
-           //$currentModel = ModelLine::model()->cache(1000, $dependency)->findByPk($model->id);
            $currentModel = ModelLine::model()->findByPk($model->id);
            if(!$currentModel->isLeaf()){
                 if(empty($title)) {
