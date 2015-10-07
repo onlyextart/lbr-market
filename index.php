615411354b9df29ef6af471cd84990059ae36991
Tatsyana Krylova <krilova@lbr.ru>
2015-10-07 15:05:11 +0300

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-error_reporting(-1);
-ini_set('display_errors', true);
+//error_reporting(-1);
+//ini_set('display_errors', true);
 
 $start = microtime(true);
 // change the following paths if necessary
@@ -9,9 +9,9 @@ $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
 
