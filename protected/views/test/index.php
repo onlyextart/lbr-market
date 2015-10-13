5c0ce4c24ccf1f9bd6808a2c4b0f48cbe7615355
Tatsyana Krylova <krilova@lbr.ru>
2015-10-13 09:59:33 +0300

    test

--- protected/views/test/index.php
+++ protected/views/test/index.php
@@ -1,13 +1,13 @@
 <?php
    //Yii::app()->clientScript->registerScriptFile('http://www.baikalsr.ru/api-calc/?ver=2.2&setAccount=BS-0000189',CClientScript::POS_END);
-   Yii::app()->clientScript->registerScriptFile('http://www.baikalsr.ru/api-calc/?ver=2.2&setAccount=BS-0000189');
+   //Yii::app()->clientScript->registerScriptFile('http://www.baikalsr.ru/api-calc/?ver=2.2&setAccount=BS-0000189');
    
    
 ?>
 
 <div id="myCalculator"></div>
 
-<script>
+<script type="text/javascript">
     $(function() {
        $("#myCalculator").bsCalculator();
    });
