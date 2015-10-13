d058b39ec8f70c531ede9000ca0545cf0b15e034
Tatsyana Krylova <krilova@lbr.ru>
2015-10-13 10:29:17 +0300

    update

--- protected/views/test/index.php
+++ protected/views/test/index.php
@@ -3,10 +3,10 @@
    Yii::app()->clientScript->registerScriptFile('http://www.baikalsr.ru/api-calc/?ver=2.2&setAccount=BS-0000189');
 ?>
 
-<div id="myCalculator"></div>
-
 <script type="text/javascript">
     $(function() {
        $("#myCalculator").bsCalculator();
-   });
+    });
 </script>
+
+<div id="myCalculator"></div>
