9f633fe8b7ca99eed635256648ae77452ea5e5ef
Tatsyana Krylova <krilova@lbr.ru>
2015-11-09 14:24:07 +0300

    teg analitics

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -230,6 +230,22 @@
                 //$this->endWidget('zii.widgets.jui.CJuiDialog');
             ?>
         </div>-->
+        <!------ Teg for remarketing ------>
+        <script type="text/javascript">
+        /* <![CDATA[ */
+        var google_conversion_id = 896868792;
+        var google_custom_params = window.google_tag_params;
+        var google_remarketing_only = true;
+        /* ]]> */
+        </script>
+        <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+        </script>
+        <noscript>
+        <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/896868792/?value=0&amp;guid=ON&amp;script=0"/>
+        </div>
+        </noscript>
+        <!------ end Teg for remarketing ------>
     </body>
 </html>
 <!----- Universal Analitics ----->
