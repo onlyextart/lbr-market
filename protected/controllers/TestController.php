09046da32fc297835a9c34b0adf2df8040e91f8d
Tatsyana Krylova <krilova@lbr.ru>
2015-11-12 12:55:45 +0300

    del unnecessary code

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -6,15 +6,4 @@ class TestController extends Controller
     {        
         $this->render('index');
     }
-    
-    public function actionRename() 
-    {        
-        $user = User::model()->findByPk(28); // WebWiki
-        $user->login = 'webviki';
-        $user->name = 'ВебВИКИ';
-        $user->email = 'marketing@webviki.by';
-        $user->phone = '+375296772782';
-        $user->password = crypt('forwebviki', User::model()->blowfishSalt());
-        $user->save();
-    }
 }
