2f0ff250645ec945b3216cb2ed6fecab6a0589f2
Tatsyana Krylova <krilova@lbr.ru>
2015-11-25 15:08:53 +0300

    commented line - show in models only original products

--- protected/models/Product.php
+++ protected/models/Product.php
@@ -228,6 +228,7 @@ class Product extends CActiveRecord {
         $criteria->join ='JOIN product_in_model_line ON product_in_model_line.product_id = t.id';
         $criteria->condition = 'product_in_model_line.model_line_id=:model_id and t.published = 1';
         $criteria->params = array(":model_id" => $this->modelLineId);
+        //$criteria->addCondition('original = 1');
         
         if(!empty($this->count)) { // for model-view filter
             if($this->count == 1) { 
