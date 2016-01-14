46d9cbd99c8482ef490e0473257300d2b01d8d70
Tatsyana Krylova <krilova@lbr.ru>
2016-01-14 16:29:55 +0300

    check for products' path

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -4,6 +4,15 @@ class TestController extends Controller
 {    
     public function actionTest() 
     {        
-        $this->render('index');
+        $products = Product::model()->findAll('id = 75959');
+        foreach($products as $product) {
+            if(empty($product->path)) {
+                $newPath = '/sparepart/'.$product->id.'-'.Translite::rusencode($product->name, '-').'/';
+                $product->path = $newPath;
+                echo $newPath.'<br>';
+                $product->save();
+                echo 'done';
+            } else echo 'no';
+        }
     }
 }
