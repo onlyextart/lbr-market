d841465bdcfec95034e329a0fdf94e6969adcec7
Tatsyana Krylova <krilova@lbr.ru>
2016-01-14 16:50:09 +0300

    update

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -4,9 +4,10 @@ class TestController extends Controller
 {    
     public function actionPathcheck() 
     {     
-        //ini_set('memory_limit', '1024M');
+        ini_set('memory_limit', '1024M');
         set_time_limit(0);
         $products = Product::model()->findAll();
+        echo count($products).'<br>';
         foreach($products as $product) {
             if(empty($product->path)) {
                 $product->path = '/sparepart/'.$product->id.'-'.Translite::rusencode($product->name, '-').'/';
