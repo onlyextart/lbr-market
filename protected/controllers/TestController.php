fe3dbd9e3dad08c7dc1a1c0e25a6bb0cd2ff2461
Tatsyana Krylova <krilova@lbr.ru>
2016-01-14 17:07:04 +0300

    check for products' path

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -4,15 +4,15 @@ class TestController extends Controller
 {    
     public function actionPathcheck() 
     {     
-        ini_set('memory_limit', '1024M');
+        ini_set('memory_limit', '16M');
         set_time_limit(0);
-        $products = Product::model()->findAll();
+        $products = Product::model()->findAll('path is null');
         echo count($products).'<br>';
         foreach($products as $product) {
-            if(empty($product->path)) {
+            //if(empty($product->path)) {
                 $product->path = '/sparepart/'.$product->id.'-'.Translite::rusencode($product->name, '-').'/';
                 $product->save();
-            }
+            //}
         }
         echo 'work done';
     }
