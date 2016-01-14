d437a052e6af1b28f5eec5bf83a0a585ddcc8c2f
Tatsyana Krylova <krilova@lbr.ru>
2016-01-14 17:30:04 +0300

    check for products' path

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -6,14 +6,28 @@ class TestController extends Controller
     {     
         //ini_set('memory_limit', '16M');
         set_time_limit(0);
-        $products = Product::model()->findAll('path is null');
-        echo count($products).'<br>';
+//        $products = Product::model()->findAll('path is null');
+//        echo count($products).'<br>';
+//        foreach($products as $product) {
+//            //if(empty($product->path)) {
+//                $product->path = '/sparepart/'.$product->id.'-'.Translite::rusencode($product->name, '-').'/';
+//                $product->save();
+//            //}
+//        }
+        
+        $sql = 'SELECT * FROM product WHERE path is null LIMIT 50';
+        $products = Product::model()->findAllBySql($sql);
+        //echo count($products).'<br>';
+        
         foreach($products as $product) {
+            echo $product->id.'<br>';
             //if(empty($product->path)) {
                 $product->path = '/sparepart/'.$product->id.'-'.Translite::rusencode($product->name, '-').'/';
                 $product->save();
             //}
         }
-        echo 'work done';
+
+        echo '======================<br>';
+        echo 'work done - '.date('H:i:s');
     }
 }
