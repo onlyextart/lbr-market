a234e0052cf5841c4b51c849a091c1d1436914de
Tatsyana Krylova <krilova@lbr.ru>
2016-01-15 13:34:59 +0300

    comment for test

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -2,36 +2,26 @@
 
 class TestController extends Controller 
 {    
-    public function actionPathcheck() 
-    {     
-        //ini_set('memory_limit', '16M');
-        set_time_limit(0);
-//        $products = Product::model()->findAll('path is null');
-//        echo count($products).'<br>';
+//    public function actionPathcheck() 
+//    {
+//        set_time_limit(0);
+//
+//        $sql = 'SELECT * FROM product WHERE path like "/sparepart/-%"';
+//        $all = Product::model()->findAllBySql($sql);
+//        echo count($all).'<br>';
+//        echo '======================<br>';
+//        
+//        $sql = 'SELECT * FROM product WHERE path like "/sparepart/-%" LIMIT 50';
+//        $products = Product::model()->findAllBySql($sql);
+//
+//        
 //        foreach($products as $product) {
-//            //if(empty($product->path)) {
-//                $product->path = '/sparepart/'.$product->id.'-'.Translite::rusencode($product->name, '-').'/';
-//                $product->save();
-//            //}
+//            echo $product->id.'<br>';
+//            $product->path = '/sparepart/'.$product->id.'-'.Translite::rusencode($product->name, '-').'/';
+//            $product->save();
 //        }
-        $sql = 'SELECT * FROM product WHERE path like "/sparepart/-%"';
-        $all = Product::model()->findAllBySql($sql);
-        echo count($all).'<br>';
-        echo '======================<br>';
-        
-        $sql = 'SELECT * FROM product WHERE path like "/sparepart/-%" LIMIT 50';
-        $products = Product::model()->findAllBySql($sql);
-
-        
-        foreach($products as $product) {
-            echo $product->id.'<br>';
-            //if(empty($product->path)) {
-                $product->path = '/sparepart/'.$product->id.'-'.Translite::rusencode($product->name, '-').'/';
-                $product->save();
-            //}
-        }
-
-        echo '======================<br>';
-        echo 'work done - '.date('H:i:s');
-    }
+//
+//        echo '======================<br>';
+//        echo 'work done - '.date('H:i:s');
+//    }
 }
