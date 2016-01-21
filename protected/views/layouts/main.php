b158464cf40d258f297d9458fe477422185ce2cf
Tatsyana Krylova <krilova@lbr.ru>
2016-01-21 10:06:12 +0300

    update for js

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -16,9 +16,9 @@
             var lbrAnaliticsMark = "<?php echo Yii::app()->params['analiticsMark']; ?>";
         </script>
         <?php
-            Yii::app()->clientScript->registerCssFile('/distribution/css/styles.min.css?48');
+            Yii::app()->clientScript->registerCssFile('/distribution/css/styles.min.css?49');
             Yii::app()->clientScript->registerCoreScript('jquery');
-            Yii::app()->clientScript->registerScriptFile('/distribution/js/scripts.min.js?5');
+            Yii::app()->clientScript->registerScriptFile('/distribution/js/scripts.min.js?6');
 
             if(empty(Yii::app()->request->cookies['lbrfilial'])) {
                 $id = Filial::model()->find('lower(name) like lower("%Москва%")')->id;
