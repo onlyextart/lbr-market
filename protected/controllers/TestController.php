fce69c2966204c23d98bef9a03491bf21fbe99e9
Tatsyana Krylova <krilova@lbr.ru>
2016-02-01 11:46:08 +0300

    update filial name

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -6,22 +6,42 @@ class TestController extends Controller
 //    {
 //        set_time_limit(0);
 //
-//        $sql = 'SELECT * FROM product WHERE path like "/sparepart/-%"';
-//        $all = Product::model()->findAllBySql($sql);
+//        $sql = 'SELECT * FROM  WHERE level=2 and path is null';
+//        $all = ProductGroupFilter::model()->findAllBySql($sql);
 //        echo count($all).'<br>';
 //        echo '======================<br>';
 //        
-//        $sql = 'SELECT * FROM product WHERE path like "/sparepart/-%" LIMIT 50';
-//        $products = Product::model()->findAllBySql($sql);
+//        $sql = 'SELECT * FROM product_group_filter WHERE level=2 and path is null LIMIT 50';
+//        $products = ProductGroupFilter::model()->findAllBySql($sql);
 //
 //        
 //        foreach($products as $product) {
 //            echo $product->id.'<br>';
-//            $product->path = '/sparepart/'.$product->id.'-'.Translite::rusencode($product->name, '-').'/';
-//            $product->save();
+//            $product->path = '/products/'.Translite::rusencode($product->name, '-');
+//            $product->saveNode();
 //        }
 //
 //        echo '======================<br>';
 //        echo 'work done - '.date('H:i:s');
 //    }
+    public function actionFilial() 
+    {
+        set_time_limit(0);
+        $filials = Filial::model()->findAll();
+
+        foreach($filials as $filial) {
+            $name = $filial->name;
+            $pos = strpos($name, "-филиал");
+            if($pos !== false) {
+                echo $filial->id.'<br>';
+                $name = substr($name, 0, $pos);
+
+                $filial->name = $name;
+                $filial->saveNode();
+            }
+        }
+
+        echo '======================<br>';
+        echo 'work done - '.date('H:i:s');
+    }
 }
