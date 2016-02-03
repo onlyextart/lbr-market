842fb4d709bb634f77ba81603e4011055837888e
Tatsyana Krylova <krilova@lbr.ru>
2016-02-03 13:42:30 +0300

    show products if sale offf

--- protected/models/Product.php
+++ protected/models/Product.php
@@ -230,8 +230,9 @@ class Product extends CActiveRecord {
         $criteria->join ='JOIN product_in_model_line ON product_in_model_line.product_id = t.id';
         $criteria->condition = 'product_in_model_line.model_line_id=:model_id and t.published = 1';
         $criteria->params = array(":model_id" => $this->modelLineId);
-        $criteria->addCondition('original = 1 and date_sale_off is null');
-        
+        //$criteria->addCondition('original = 1 and date_sale_off is null');
+        $criteria->addCondition('original = 1');
+      
         if(!empty($this->count)) { // for model-view filter
             if($this->count == 1) { 
                 $criteria->addCondition('count > 0');
