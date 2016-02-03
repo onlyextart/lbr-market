1274e23eef0575a26af40777727bf9d1df7c53df
Tatsyana Krylova <krilova@lbr.ru>
2016-02-03 10:12:16 +0300

    update

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -2,28 +2,28 @@
 
 class TestController extends Controller 
 {    
-    public function actionPathcheck() 
-    {
-        set_time_limit(0);
-
-        $sql = 'SELECT * FROM product_group_filter WHERE level=2 and path is null';
-        $all = ProductGroupFilter::model()->findAllBySql($sql);
-        echo count($all).'<br>';
-        echo '======================<br>';
-        
-        $sql = 'SELECT * FROM product_group_filter WHERE level=2 and path is null LIMIT 50';
-        $products = ProductGroupFilter::model()->findAllBySql($sql);
-
-        
-        foreach($products as $product) {
-            echo $product->id.'<br>';
-            $product->path = '/products/'.Translite::rusencode($product->name, '-');
-            $product->saveNode();
-        }
-
-        echo '======================<br>';
-        echo 'work done - '.date('H:i:s');
-    }
+//    public function actionPathcheck() 
+//    {
+//        set_time_limit(0);
+//
+//        $sql = 'SELECT * FROM product_group_filter WHERE level=2 and path is null';
+//        $all = ProductGroupFilter::model()->findAllBySql($sql);
+//        echo count($all).'<br>';
+//        echo '======================<br>';
+//        
+//        $sql = 'SELECT * FROM product_group_filter WHERE level=2 and path is null LIMIT 50';
+//        $products = ProductGroupFilter::model()->findAllBySql($sql);
+//
+//        
+//        foreach($products as $product) {
+//            echo $product->id.'<br>';
+//            $product->path = '/products/'.Translite::rusencode($product->name, '-');
+//            $product->saveNode();
+//        }
+//
+//        echo '======================<br>';
+//        echo 'work done - '.date('H:i:s');
+//    }
     
     /*public function actionFilial() 
     {
