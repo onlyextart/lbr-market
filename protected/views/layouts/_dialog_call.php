0f0f9ee5c926cc9ee9309afe60ab164c201c9a40
teterukova <teterukova@lbr.ru>
2016-02-19 15:14:13 +0300

    phone example

--- protected/views/layouts/_dialog_call.php
+++ protected/views/layouts/_dialog_call.php
@@ -9,7 +9,7 @@
             <form method="POST" action="http://customer.voipexchange.ru/cgi-bin/Exchange.dll/FreeWebCall">
                 <input type=hidden name="ID" value="92465201823405422891005848819646">
                 <input type=hidden name="Delay" value="0">
-                <div class="row"><label for="phone">Ваш телефон, включая код страны и города:</label></div>
+                <div class="row"><label for="phone">Ваш телефон, включая код страны и города<br> <span class='example'>например, 79102345678</span></label></div>
                 <div class="row"><input type="text" name="phone" maxlength="32" size="16"></div>
                 <div class="row button"><input class="buttonform" type="submit" value="ПОЗВОНИТЬ"></div>
             </form>
