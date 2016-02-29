5f2d43e6fc78dd4581387eb033d3847c898191f1
Tatsyana Krylova <krilova@lbr.ru>
2016-02-29 15:22:48 +0300

    update

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -71,9 +71,10 @@ class TestController extends Controller
     
     public function actionTest()
     {
-        //phpinfo(); 
-        if(in_array('mod_rewrite', apache_get_modules())) echo 'on';
-        else echo 'off';
+        if(in_array('mod_rewrite', apache_get_modules())) 
+                echo 'on';
+        else 
+            echo 'off';
         
         exit;
     }
