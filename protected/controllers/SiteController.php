7794f4f1379a72dbbd3b0eaa38ae7ed4a7321ddd
Tatsyana Krylova <krilova@lbr.ru>
2016-03-23 13:51:58 +0300

    add sitemap.xml

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2,15 +2,14 @@
 
 class SiteController extends Controller {
 
-    public function actionIndex($s = null) 
-    {
+    public function actionIndex($s = null) {
         $makers = $this->getMakers();
         $bestOffer = $this->getBestOffer();
         $hitProducts = $this->getHitProducts();
         Yii::app()->params['meta_description'] = Yii::app()->params['meta_title'];
         $this->render('index', array('hitProducts' => $hitProducts, 'bestoffer' => $bestOffer, 'makers' => $makers));
     }
-    
+
     public function getMakers() {
         $result = '';
         $equipmentMakers = EquipmentMaker::model()->getAllMakers();
@@ -125,13 +124,12 @@ class SiteController extends Controller {
         return $result;
     }
 
-    public function actionDescription($url) 
-    {
+    public function actionDescription($url) {
         $model = Page::model()->findByAttributes(array('url' => $url));
         Yii::app()->params['meta_description'] = Yii::app()->params['meta_title'] = $breadcrumbs[] = $model->title;
         //Yii::app()->params['breadcrumbs'] = $breadcrumbs;
-        
-        $this->render('staticPage', array('data' => $model, 'url'=>$url), false, true);
+
+        $this->render('staticPage', array('data' => $model, 'url' => $url), false, true);
     }
 
     public function actions() {
@@ -507,4 +505,34 @@ class SiteController extends Controller {
 
         echo json_encode($filials);
     }
+
+    public function actionSaveSitemap() {
+        $sitemap = '<?xml version="1.0" encoding="UTF-8"?>
+            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">'
+        ;
+        
+        $menuItems = Yii::app()->db->createCommand("SELECT path from product where published='1'")->queryAll();
+        foreach ($menuItems as $menuItem) {
+            $path = $menuItem['path'];
+            $find = strpos($path, 'www');
+
+            if ($find) {
+                $path = 'http://' . substr($path, $find);
+                $sitemap .='<url><loc>' . $path . '/</loc></url>';
+            } else {
+                if ($path == '/index') {
+                    $path = '';
+                }
+                $sitemap .='<url><loc>http://lbr-market.ru' . $path . '</loc></url>';
+            }
+        }
+        $sitemap .='</urlset>';
+        file_put_contents('sitemap.xml', $sitemap);
+        //$this->redirect('/administrator/menu/sitemap');
+        echo 'complete';
+    }
+
 }
