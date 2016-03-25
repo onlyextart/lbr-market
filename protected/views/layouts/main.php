e846e0432f1c84816887965e58ea99404db615e9
Tatsyana Krylova <krilova@lbr.ru>
2016-03-25 15:28:32 +0300

    comment online saller

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -244,7 +244,7 @@
             </div>
         </div-->
         <!-- OnlineSeller.ru {literal} -->
-             <script type="text/javascript">var _oaq = _oaq || [];_oaq.push(['_OPAccount', '716']);(function() {var oa = document.createElement('script'); oa.type = 'text/javascript';oa.charset='UTF-8'; oa.async = true; oa.src = 'http://onlinesaler.ru/assets/templates/os2013/common/js.php?akkid=716'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(oa, s);  })();</script>
+             <!--script type="text/javascript">var _oaq = _oaq || [];_oaq.push(['_OPAccount', '716']);(function() {var oa = document.createElement('script'); oa.type = 'text/javascript';oa.charset='UTF-8'; oa.async = true; oa.src = 'http://onlinesaler.ru/assets/templates/os2013/common/js.php?akkid=716'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(oa, s);  })();</script-->
         <!--OnlineSeller.ru {/literal} -->
                                         
 
