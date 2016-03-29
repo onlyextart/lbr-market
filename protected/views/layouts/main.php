ec2d1803f9701437570c7473ed2fd1e43f25d405
Tatsyana Krylova <krilova@lbr.ru>
2016-03-29 16:29:46 +0300

    JivoSite for admin

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -253,7 +253,15 @@
             var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script-->
         <!-- {/literal} END JIVOSITE CODE -->
 
-                                        
+        <!-- Test -->
+        <?php if(!Yii::app()->user->isGuest && empty(Yii::app()->user->isShop)): // admin ?>
+            <script type='text/javascript'>
+            (function(){ var widget_id = 'ghN05azA7B';
+            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+        <?php else: ?>
+        <script type="text/javascript">var _oaq = _oaq || [];_oaq.push(['_OPAccount', '716']);(function() {var oa = document.createElement('script'); oa.type = 'text/javascript';oa.charset='UTF-8'; oa.async = true; oa.src = 'http://onlinesaler.ru/assets/templates/os2013/common/js.php?akkid=716'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(oa, s);  })();</script>
+        <?php endif; ?>
+        <!-- Test -->                                
 
 <!--        <div>
             <?php //$this->beginWidget('zii.widgets.jui.CJuiDialog', array(
