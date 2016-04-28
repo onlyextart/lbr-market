bbcdbe841f7c878ea26ddad8619bdd809dadf8bc
Tatsyana Krylova <krilova@lbr.ru>
2016-04-28 10:53:25 +0300

    update

--- protected/controllers/ModellinesController.php
+++ protected/controllers/ModellinesController.php
@@ -274,18 +274,18 @@ class ModellinesController extends Controller
 //                    ;
                     
                     $name = EquipmentMaker::model()->findByPk($brand->maker_id)->name;
--                   $modellines_of_brand= Yii::app()->db->createCommand()
--                        ->selectDistinct('id')
--                        ->from('model_line')
--                        ->where('maker_id = :maker_id and category_id = :category_id and level = 2', array(':maker_id' => $brand->maker_id, ':category_id' => $categoryId))
+                   $modellines_of_brand= Yii::app()->db->createCommand()
+                        ->selectDistinct('id')
+                        ->from('model_line')
+                        ->where('maker_id = :maker_id and category_id = :category_id and level = 2', array(':maker_id' => $brand->maker_id, ':category_id' => $categoryId))
                     ->queryColumn();
 
                    $result[][$name] =$modellines_of_brand;
--                   if(isset($top_makers)&&!empty($top_makers)){
--                       if(in_array($brand->maker_id, $top_makers)){
--                            $result_top[][$name]=$modellines_of_brand;
--                       }
--                  }  
+                   if(isset($top_makers)&&!empty($top_makers)){
+                       if(in_array($brand->maker_id, $top_makers)){
+                            $result_top[][$name]=$modellines_of_brand;
+                       }
+                  }  
 
                     if(!empty($modellines_of_brand)) {
                         $result[][$name] = $modellines_of_brand;
